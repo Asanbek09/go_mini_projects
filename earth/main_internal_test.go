fc54c4578078e537f5e2f2c40272a6004950dd07
Asanbek09 <samudinua@gmail.com>
2025-02-05 22:12:28 +0600

    main_internal_test изменения кода

--- earth/main_internal_test.go
+++ earth/main_internal_test.go
@@ -2,10 +2,37 @@ package main
 
 import "testing"
 
-func TestGreet(t *testing.T) {
-	want := "Hello world!!!!"
+func ExampleMain(){
+	main()
+}
 
-	got := greet()
+func TestGreet_English(t *testing.T) {
+	lang := language("en")
+	want := "Hello World!!!!"
+
+	got := greet(lang)
+
+	if got != want {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le mon"
+
+	got := greet(lang)
+
+	if got != want {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_Akkadian(t *testing.T) {
+	lang := language("akk")
+	want := ""
+
+	got := greet(lang)
 
 	if got != want {
 		t.Errorf("expected: %q, got: %q", want, got)
