dfd359a7cba90ad5eb0c59ad87744f0037072771
Asanbek09 <samudinua@gmail.com>
2025-02-05 21:24:54 +0600

    main_internal_test.go изменения

--- earth/main_internal_test.go
+++ earth/main_internal_test.go
@@ -1,5 +1,13 @@
 package main
 
-func ExampleMain() {
-	main()
+import "testing"
+
+func TestGreet(t *testing.T) {
+	want := "Hello world!!!!"
+
+	got := greet()
+
+	if got != want {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
 }
