5d4eb34a28ca786001786645bd4919af7336ae8e
Asanbek09 <samudinua@gmail.com>
2025-02-05 22:17:13 +0600

    main.go изменения по коду

--- earth/main.go
+++ earth/main.go
@@ -9,13 +9,20 @@ func main() {
 
 type language string
 
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε", // greek
+	"en": "Hello World", // english
+	"fr": "Bonjour le monde", // french
+	"he": "שלום עולם", // hebrew
+	"ur": "ہیلو دنیا", // urdu
+	"vi": "Xin chào Thế Giới", // vietnamese
+}
+
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello World!!!!"
-	case "fr":
-		return "Bonjour le monde"
-	default:
-		return ""
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
 	}
+
+	return greeting
 }
