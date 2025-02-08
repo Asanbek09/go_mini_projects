cfb8f8e41c40be440c99f676a8a83169e5978a8a
Asanbek09 <samudinua@gmail.com>
2025-02-08 11:08:28 +0600

    Небольшие изменения в *_test.go

--- bookworm/bookworms_internal_test.go
+++ bookworm/bookworms_internal_test.go
@@ -1,8 +1,16 @@
 package main
 
+import "testing"
 
-type testCase struct {
-	bookwormsFile string
-	want []Bookworm
-	wantErr bool
+func TestLoadBookworms_Success(t *testing.T) {
+	type testCase struct {
+		bookwormsFile string
+		want []Bookworm
+		wantErr bool
+	}
 }
+
+var (
+	handmaidsTale = Book{Author: "Margaret Atwwod", Title: "The Handmaid's Tale"}
+	oryxAndCrake = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
+)
