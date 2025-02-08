0302d958df2b0fc4cf193b9c9a780f6d4b5d0171
Asanbek09 <samudinua@gmail.com>
2025-02-08 11:15:07 +0600

    Создана функция TestLoadBookworms_Success

--- bookworm/bookworms_internal_test.go
+++ bookworm/bookworms_internal_test.go
@@ -2,15 +2,38 @@ package main
 
 import "testing"
 
+var (
+	handmaidsTale = Book{Author: "Margaret Atwwod", Title: "The Handmaid's Tale"}
+	oryxAndCrake = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
+	theBellJar = Book{Author: "Sylvia Plath", Title: "The Bell Jar"}
+	janeEyre = Book{Author: "Charlotte Bronte", Title: "Jane Eyre"}
+)
+
 func TestLoadBookworms_Success(t *testing.T) {
 	type testCase struct {
 		bookwormsFile string
 		want []Bookworm
 		wantErr bool
 	}
-}
 
-var (
-	handmaidsTale = Book{Author: "Margaret Atwwod", Title: "The Handmaid's Tale"}
-	oryxAndCrake = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
-)
+	tests := map[string]testCase{
+		"file exists": {
+			bookwormsFile: "data.json",
+			want: []Bookworm{
+				{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
+				{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, janeEyre}},
+			},
+			wantErr: false,
+		},
+		"file doesn't exist": {
+			bookwormsFile: "no_file_here.json",
+			want: nil,
+			wantErr: true,
+		},
+		"invalid JSON": {
+			bookwormsFile: "invalid.json",
+			want: nil,
+			wantErr: true,
+		},
+	}
+}
