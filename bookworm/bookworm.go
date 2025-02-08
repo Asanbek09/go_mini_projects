f4c7612178f635816ef1bd292d108e60ea34bf54
Asanbek09 <samudinua@gmail.com>
2025-02-08 10:46:25 +0600

    os - импорт

--- bookworm/bookworm.go
+++ bookworm/bookworm.go
@@ -1,10 +1,20 @@
 package main
 
+import "os"
+
 type Bookworm struct {
 	Name string
 	Books string
 }
 
 func loadBookworms(filePath string) ([]Bookworm, error) {
+	
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
 	return nil, nil
 }
+
