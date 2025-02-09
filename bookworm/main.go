dd6982ba30953e81bf14beeddffae38274b5ad73
Asanbek09 <samudinua@gmail.com>
2025-02-09 14:47:37 +0600

    main.go небольшие изменения

--- bookworm/main.go
+++ bookworm/main.go
@@ -8,9 +8,12 @@ import (
 func main() {
 	bookworms, err := loadBookworms("database/data.json")
 	if err != nil {
-		_, _ =fmt.Fprintf(os.Stderr, "failed to load bookworms: %s\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "failed to load bookworms: %s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(bookworms)
+	commonBooks := findCommonBooks(bookworms)
+
+	fmt.Println("Here are the books in common:")
+	displayBooks(commonBooks)
 }
