6f2c73a99e106622b3f6153aaeeb490fc995d9d6
Asanbek09 <samudinua@gmail.com>
2025-02-09 14:41:31 +0600

    sortBooks добавлен в  bookworm.go

--- bookworm/bookworm.go
+++ bookworm/bookworm.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"sort"
 )
 
 type Bookworm struct {
@@ -56,4 +57,15 @@ func booksCount(bookworms []Bookworm) map[Book]uint {
 	}
 
 	return count
+}
+
+func sortBooks(books []Book) []Book {
+	sort.Slice(books, func(i, j int) bool {
+		if books[i].Author != books[j].Author {
+			return books[i].Author < books[j].Author
+		}
+		return books[i].Title < books[j].Title
+	})
+
+	return books
 }
