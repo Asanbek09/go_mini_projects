11f4f499b8186555d12cc294f8c4d6a357de23ad
Asanbek09 <samudinua@gmail.com>
2025-02-10 09:18:00 +0600

    recommendations.go файл создан

---
+++ bookworm/recommendations.go
@@ -0,0 +1,29 @@
+package main
+
+func recommendOtherBooks(bookworms []Bookworm) []Bookworm {
+	sh := make(bookRecommendations)
+
+	for _, bookworm := range bookworms {
+		for i, book := range bookworm.Books {
+			otherBooksOnShelves := listOtherBooksOnShelves(i, bookworm.Books)
+			registerBookRecommendations(sb, book, otherBooksOnShelves)
+		}
+	}
+
+	recommendations := make([]Bookworm, len(bookworms))
+	for i, bookworm := range bookworms {
+		recommendations[i] = Bookworm{
+			Name: bookworm.Name,
+			Books: recommendBooks(sb, bookworm.Books),
+		}
+	}
+
+	return recommendations
+}
+
+type set map[Book] struct {}
+
+func (s set) Contains(b Book) bool {
+	_, ok := s[b]
+	return ok
+}
