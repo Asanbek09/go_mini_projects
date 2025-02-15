15ce760f86491c6c78410839fe08c0bcec8f0ee9
Asanbek09 <samudinua@gmail.com>
2025-02-15 15:17:46 +0600

    main.go изменен

--- logger/main.go
+++ logger/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"os"
-
+	"time"
 	"logger/log"
 )
 
@@ -10,4 +10,8 @@ func main() {
 	lgr := log.New(log.LeveInfo, log.WithOutput(os.Stdout))
 
 	lgr.Infof("A little copying is better than a little dependency")
+	lgr.Errorf("Errors are values. Documentation is for %s.", "users")
+	lgr.Debugf("Make the zero (%d) value useful", 0)
+
+	lgr.Infof("Hallo, %d %v", 2022, time.Now())
 }
