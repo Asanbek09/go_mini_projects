82b7b070c934d7ee6b56be18eba46088d5aacc45
Asanbek09 <samudinua@gmail.com>
2025-02-15 15:10:07 +0600

    main.go пишем код

--- logger/main.go
+++ logger/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"os"
+
+	"logger/log"
+)
+
+func main() {
+	lgr := log.New(log.LeveInfo, log.WithOutput(os.Stdout))
+
+	lgr.Infof("A little copying is better than a little dependency")
+}
