b373b4e687db82102d2b25fed8e227bf68bb1187
Asanbek09 <samudinua@gmail.com>
2025-02-15 15:29:29 +0600

    logger_test.go -> testWriter добавлен

--- logger/log/logger_test.go
+++ logger/log/logger_test.go
@@ -3,7 +3,17 @@ package log_test
 import (
 	"logger/log"
 )
+
+type testWriter struct {
+	contents string
+}
+
 func ExampleLogger_Debugf() {
 	debugLogger := log.New(log.LevelDebug)
 	debugLogger.Debugf("Hello, %s", "world")
+}
+
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+	tw.contents = tw.contents + string(p)
+	return len(p), nil
 }
