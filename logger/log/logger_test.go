93d2cf96c47ddd3642122e1cce941d45b1447876
Asanbek09 <samudinua@gmail.com>
2025-02-15 15:59:24 +0600

    logger_test.go -> TestLogger_DebugfInfoErrorf добавлен

--- logger/log/logger_test.go
+++ logger/log/logger_test.go
@@ -2,6 +2,7 @@ package log_test
 
 import (
 	"logger/log"
+	"testing"
 )
 
 const (
@@ -22,4 +23,42 @@ func ExampleLogger_Debugf() {
 func (tw *testWriter) Write(p []byte) (n int, err error) {
 	tw.contents = tw.contents + string(p)
 	return len(p), nil
+}
+
+func TestLogger_DebugfInfoErrorf(t *testing.T) {
+	type testCase struct {
+		level log.Level
+		expected string
+	}
+
+	tt := map[string]testCase{
+		"debug": {
+			level: log.LevelDebug,
+			expected: debugMessage + "\n" + infoMessage + "\n" + errorMessage + "\n",
+		},
+		"info": {
+			level: log.LeveInfo,
+			expected: infoMessage + "\n" + errorMessage + "\n",
+		},
+		"error": {
+			level: log.LevelError,
+			expected: errorMessage + "\n",
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			tw := &testWriter{}
+
+			testedLogger := log.New(tc.level, log.WithOutput(tw))
+
+			testedLogger.Debugf(debugMessage)
+			testedLogger.Infof(infoMessage)
+			testedLogger.Errorf(errorMessage)
+
+			if tw.contents != tc.expected {
+				t.Errorf("Invalid contents, expected %q, got %q", tc.expected, tw.contents)
+			}
+		})
+	}
 }
