2524e02cc6bf0dcec199ba7d7afa397a722053b0
Asanbek09 <samudinua@gmail.com>
2025-02-16 11:07:08 +0600

    Функция Play создана внутри game.go

--- gordle/gordle_packs/game.go
+++ gordle/gordle_packs/game.go
@@ -1,9 +1,17 @@
 package gordlepacks
 
+import "fmt"
+
 type Game struct {}
 
 func New() *Game {
 	g := &Game{}
 
 	return g
+}
+
+func (g *Game) Play() {
+	fmt.Println("Welcome to Gordle!")
+
+	fmt.Printf("Enter a guess : \n")
 }
