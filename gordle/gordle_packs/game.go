72865e10bd7269c04a20265b3f9f0430b7fabfa3
Asanbek09 <samudinua@gmail.com>
2025-02-16 12:42:37 +0600

    game.go создана функция ask

--- gordle/gordle_packs/game.go
+++ gordle/gordle_packs/game.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 )
 
 type Game struct {
@@ -22,4 +23,26 @@ func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 
 	fmt.Printf("Enter a guess : \n")
+}
+
+const solutionLength = 5
+
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess: \n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %\n", err.Error())
+			continue
+		}
+
+		guess := []rune(string(playerInput))
+
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution! Expected %d characters, got %d\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
