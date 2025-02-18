ede3915239f70d1781bbd63180381e736dccd6e4
Asanbek09 <samudinua@gmail.com>
2025-02-18 14:46:30 +0600

    hint_internal_test.go создан

---
+++ gordle/gordle_packs/hint_internal_test.go
@@ -0,0 +1,35 @@
+package gordlepacks
+
+import "testing"
+
+func Test_feedback_String(t *testing.T) {
+	testCases := map[string]struct {
+		fb feedback
+		want string
+	}{
+		"three correct": {
+			fb: feedback{correctPosition, correctPosition, correctPosition},
+			want: "🎉🎉🎉",
+		},
+		"one of each": {
+			fb: feedback{correctPosition, wrongPosition, absentCharacter},
+			want: "🎉🤡🥳",
+		},
+		"different order for one of each": {
+			fb: feedback{wrongPosition, absentCharacter, correctPosition},
+			want: "🤡🥳🎉",
+		},
+		"unknown position": {
+			fb: feedback{wrongPosition, wrongPosition, wrongPosition},
+			want: "😅",
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			if got := testCase.fb.String(); got != testCase.want {
+				t.Errorf("String() = %v, want %v", got, testCase.want)
+			}
+		})
+	}
+}
