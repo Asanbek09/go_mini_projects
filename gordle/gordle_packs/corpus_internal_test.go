e1c3cdb81ae571f8e93b0feb9f7e96b6f433a397
Asanbek09 <samudinua@gmail.com>
2025-02-19 13:57:52 +0600

    corpus_internal_test.go -> TestPickWord

--- gordle/gordle_packs/corpus_internal_test.go
+++ gordle/gordle_packs/corpus_internal_test.go
@@ -1,5 +1,7 @@
 package gordlepacks
 
+import "testing"
+
 func inCorpus(corpus []string, word string) bool {
 	for _, corpusWord := range corpus {
 		if corpusWord == word {
@@ -8,4 +10,13 @@ func inCorpus(corpus []string, word string) bool {
 	}
 
 	return false
+}
+
+func TestPickWord(t *testing.T) {
+	corpus := []string{"HELLO", "SALUT", "ПРИВЕТ", "XAIPE"}
+	word := pickWord(corpus)
+
+	if !inCorpus(corpus, word) {
+		t.Errorf("Expected a word in the corpus, got %q", word)
+	}
 }
