63c7b3e13c1ed22eea54bd178c81ee76143f32ca
Asanbek09 <samudinua@gmail.com>
2025-02-19 14:37:08 +0600

    main.go последние изменения

--- gordle/main.go
+++ gordle/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"gordle/gordle_packs"
 	"os"
-    "gordle/gordle_packs"
 )
 
 const maxAttempts = 6
 
 func main() {
-    solution := "Hello"
+    corpus, err := gordlepacks.ReadCorpus("corpus/english.txt")
+    if err != nil {
+        _, _ = fmt.Fprintf(os.Stderr, "unable to read corpus: %s", err)
+        return
+    }
 
-    g := gordlepacks.New(os.Stdin, solution, maxAttempts)
+    g, err := gordlepacks.New(bufio.NewReader(os.Stdin), corpus, maxAttempts)
+    if err != nil {
+        _, _ = fmt.Fprintf(os.Stderr, "unable to start game: %s", err)
+        return
+    }
+
     g.Play()
 }
