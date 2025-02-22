365aaaa6b1c8830e33f46309dacd5b32282b2ed6
Asanbek09 <samudinua@gmail.com>
2025-02-22 12:51:07 +0600

    decimal.go добавлены константы

--- converter/money/decimal.go
+++ converter/money/decimal.go
@@ -1,6 +1,12 @@
 package money
 
 type Decimal struct {
-	subunits int64
+	subunits  int64
 	precision byte
 }
+
+const (
+	ErrInvalidDecimal = Error("unable to convert the decimal")
+
+	ErrTooLarge = Error("quantity over 10^12 is too large")
+)
