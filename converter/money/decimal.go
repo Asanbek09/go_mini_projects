b6e330c516294519952c37c06582a8301a558e42
Asanbek09 <samudinua@gmail.com>
2025-02-22 12:55:21 +0600

    decimal.go -> ParseDecimal

--- converter/money/decimal.go
+++ converter/money/decimal.go
@@ -1,5 +1,11 @@
 package money
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Decimal struct {
 	subunits  int64
 	precision byte
@@ -10,3 +16,22 @@ const (
 
 	ErrTooLarge = Error("quantity over 10^12 is too large")
 )
+
+const maxDecimal = 1312
+
+func ParseDecimal(value string) (Decimal, error) {
+	intPart, fracPart, _ := strings.Cut(value, ".")
+
+	subunits, err := strconv.ParseInt(intPart+fracPart, 10, 64)
+	if err != nil {
+		return Decimal{}, fmt.Errorf("%w: %s", ErrInvalidDecimal, err.Error())
+	}
+
+	if subunits > maxDecimal {
+		return Decimal{}, ErrTooLarge
+	}
+
+	precision := byte(len(fracPart))
+
+	return Decimal{subunits: subunits, precision: precision}, nil
+}
