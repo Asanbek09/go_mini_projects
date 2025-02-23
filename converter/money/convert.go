0021b1a6254ed1590defc3067da5e0edd8e024af
Asanbek09 <samudinua@gmail.com>
2025-02-23 16:40:03 +0600

    convert.go -> pow10

--- converter/money/convert.go
+++ converter/money/convert.go
@@ -1,5 +1,24 @@
 package money
 
+import "math"
+
 func Convert(amount Amount, to Currency) (Amount, error) {
 	return Amount{}, nil
+}
+
+type ExchangeRate Decimal
+
+func pow10(power byte) int64 {
+	switch power {
+	case 0:
+		return 1
+	case 1:
+		return 10
+	case 2:
+		return 100
+	case 3:
+		return 1000
+	default:
+		return int64(math.Pow(10, float64(power)))
+	}
 }
