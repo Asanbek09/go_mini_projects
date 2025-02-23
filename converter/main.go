5b5cf2f8b144d747ec90ea0daff1c5a6d59d9da5
Asanbek09 <samudinua@gmail.com>
2025-02-23 18:03:04 +0600

    main.go - изменения

--- converter/main.go
+++ converter/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"converter/money"
 	"flag"
 	"fmt"
 	"os"
@@ -12,6 +13,18 @@ func main() {
 
 	flag.Parse()
 
+	fromCurrency, err := money.ParseCurrency(*from)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse source currency %q: %s \n", *from, err.Error())
+		os.Exit(1)
+	}
+
+	toCurrency, err := money.ParseCurrency(*to)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse target currency %q: %s\n", *to, err.Error())
+		os.Exit(1)
+	}
+
 	value := flag.Arg(0)
 	if value == "" {
 		_, _ = fmt.Fprintln(os.Stderr, "missing amount to convert")
@@ -19,5 +32,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(*from, *to, value)
+	quantity, err := money.ParseDecimal(value)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse value %q : %s\n", value, err.Error())
+		os.Exit(1)
+	}
+
+	amount, err := money.NewAmount(quantity, fromCurrency)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	convertedAmount, err := money.Convert(amount, toCurrency)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to convert %s to %s: %s\n", amount, toCurrency, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s = %s\n", amount, convertedAmount)
 }
