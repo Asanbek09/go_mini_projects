76b9a80a3e045a977687ba877fc44eb7fe1d7809
Asanbek09 <samudinua@gmail.com>
2025-02-23 17:36:28 +0600

    main.go изменения

--- converter/main.go
+++ converter/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	from := flag.String("from", "", "source currency, required")
+	to := flag.String("to", "EUR", "target currency")
+
+	flag.Parse()
+
+	fmt.Println(*from, *to)
+}
