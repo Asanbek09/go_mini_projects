30a976a77ee2325f6ae1b9134de0319c114527a4
Asanbek09 <samudinua@gmail.com>
2025-02-24 10:48:52 +0600

    ecb.go изменения

--- converter/ecbank/ecb.go
+++ converter/ecbank/ecb.go
@@ -1,9 +1,29 @@
 package ecbank
 
-import "converter/money"
+import (
+	"converter/money"
+	"net/http"
+	"fmt"
+)
 
-type Client struct{}
+
+type Client struct{
+	url string
+}
 
 func (c Client) FetchExchangeRate(source, target money.Currency) (money.ExchangeRate, error) {
+	const euroxrefURL = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+
+	if c.url == "" {
+		c.url = euroxrefURL
+	}
+
+	resp, err := http.Get(c.url)
+	if err != nil {
+		return money.ExchangeRate{}, fmt.Errorf("%w: %s", ErrServerSide, err.Error())
+}
 	return money.ExchangeRate{}, nil
 }
+
+
+
