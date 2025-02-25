c2f0731b3057529552eb6a3c429a3d6e72e9596c
Asanbek09 <samudinua@gmail.com>
2025-02-25 09:22:52 +0600

    envelope.go -> exchangeRates

--- converter/ecbank/envelope.go
+++ converter/ecbank/envelope.go
@@ -10,3 +10,17 @@ type currencyRate struct {
 	Currency string `xml:"currency,attr"`
 	Rate money.ExchangeRate `xml:"rate,attr"`
 }
+
+const baseCurrencyCode = "EUR"
+
+func (e envelope) exchangeRates() map[string]money.ExchangeRate {
+	rates := make(map[string]money.ExchangeRate, len(e.Rates)+1)
+
+	for _, c := range e.Rates {
+		rates[c.Currency] = c.Rate
+	}
+
+	rates[baseCurrencyCode] = 1.
+
+	return rates
+}
