4d94c7e14781a829d4374a8a34c3c948d17c8c4b
Asanbek09 <samudinua@gmail.com>
2025-02-25 09:30:50 +0600

    envelope.go -> exchangeRate

--- converter/ecbank/envelope.go
+++ converter/ecbank/envelope.go
@@ -1,6 +1,9 @@
 package ecbank
 
-import "converter/money"
+import (
+	"converter/money"
+	"fmt"
+)
 
 type envelope struct {
 	Rates []currencyRate `xml:"Cube>Cube>Cube"`
@@ -23,4 +26,24 @@ func (e envelope) exchangeRates() map[string]money.ExchangeRate {
 	rates[baseCurrencyCode] = 1.
 
 	return rates
+}
+
+func (e envelope) exchangeRate(source, target string) (money.ExchangeRate, error) {
+	if source == target {
+		return 1., nil
+	}
+
+	rates := e.mappedChangeRates()
+
+	sourceFactor, sourceFound := rates[source]
+	if !sourceFound {
+		return 0, fmt.Errorf("failed to find the source currency %s", source)
+	}
+
+	targetFactor, targetFound := rates[target]
+	if !targetFound {
+		return 0, fmt.Errorf("failed to find target currency %s", target)
+	}
+
+	return targetFactor / sourceFactor, nil
 }
