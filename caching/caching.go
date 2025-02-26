802f3d2d5853ad7971ad843065057684597dd9cc
Asanbek09 <samudinua@gmail.com>
2025-02-26 11:38:56 +0600

    prettyPrint2

--- caching/caching.go
+++ caching/caching.go
@@ -6,6 +6,13 @@ func prettyPrint(f float64) {
 	fmt.Printf("> %f", f)
 }
 
+func prettyPrint2[T any](t T) {
+	fmt.Printf("> %v", t)
+}
+
 func main() {
 	prettyPrint(.25)
+
+	prettyPrint2[float64](.25)
+	prettyPrint2[string]("pockets")
 }
