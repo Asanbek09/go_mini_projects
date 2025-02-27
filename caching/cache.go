e851d459f6b25ce41be65389a0c514cdaba57364
Asanbek09 <samudinua@gmail.com>
2025-02-27 14:45:03 +0600

    cache.go -> Upsert

--- caching/cache.go
+++ caching/cache.go
@@ -1,4 +1,4 @@
-package main
+package caching
 
 type Cache[K comparable, V any] struct {
 	data map[K]V
@@ -13,4 +13,10 @@ func New[K comparable, V any]() Cache[K, V] {
 func (c *Cache[K, V]) Read(key K) (V, bool) {
 	v, found := c.data[key]
 	return v, found
+}
+
+func (c *Cache[K, V]) Upsert(key K, value V) error {
+	c.data[key] = value
+
+	return nil
 }
