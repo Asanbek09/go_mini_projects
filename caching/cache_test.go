4244c6dcf61ab5a8563de08ef53aef004e9a50c3
Asanbek09 <samudinua@gmail.com>
2025-02-27 16:34:57 +0600

    cache_test.go прописан

--- caching/cache_test.go
+++ caching/cache_test.go
@@ -15,4 +15,30 @@ func TestCache(t *testing.T) {
 	v, found := c.Read(5)
 	assert.True(t, found)
 	assert.Equal(t, "funf", v)
+
+	c.Upsert(5, "pum")
+
+	v, found = c.Read(5)
+	assert.True(t, found)
+	assert.Equal(t, "pum", v)
+
+	c.Upsert(3, "drei")
+
+	v, found = c.Read(3)
+	assert.True(t, found)
+	assert.Equal(t, "drei", v)
+
+	v, found = c.Read(5)
+	assert.True(t, found)
+	assert.Equal(t, "pum", v)
+
+	c.Delete(5)
+
+	v, found = c.Read(5)
+	assert.False(t, found)
+	assert.Equal(t, "", v)
+
+	v, found = c.Read(3)
+	assert.True(t, found)
+	assert.Equal(t, "drei", v)
 }
