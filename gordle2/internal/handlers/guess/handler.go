5e9064d4f447b80c68f8df6c09b7c4477ca8d06a
Asanbek09 <samudinua@gmail.com>
2025-03-01 17:04:09 +0600

    guess/handler.go изменения

--- gordle2/internal/handlers/guess/handler.go
+++ gordle2/internal/handlers/guess/handler.go
@@ -3,15 +3,32 @@ package guess
 import (
 	"encoding/json"
 	"gordle2/internal/api"
+	"log"
 	"net/http"
 )
 
 func Handle(w http.ResponseWriter, req *http.Request) {
+
+	id := req.PathValue(api.GameID)
+	if id == "" {
+		http.Error(w, "missing th id of the game", http.StatusBadRequest)
+		return
+	}
 
 	r := api.GuessRequest{}
 	err := json.NewDecoder(req.Body).Decode(&r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
+	}
+
+	apiGame := api.GameResponse{
+		ID: id,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(apiGame)
+	if err != nil {
+		log.Printf("failed to write response: %s", err)
 	}
 }
