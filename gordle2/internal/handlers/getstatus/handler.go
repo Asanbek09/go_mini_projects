f86e39b61c754cbfb930fc9bf973dcf8b0a33e8b
Asanbek09 <samudinua@gmail.com>
2025-03-01 16:47:21 +0600

    handlers/getstatus -> handler_test.go

--- gordle2/internal/handlers/getstatus/handler.go
+++ gordle2/internal/handlers/getstatus/handler.go
@@ -8,7 +8,6 @@ import (
 	"gordle2/internal/api"
 )
 
-// Handle is the handler for the status retrieval endpoint.
 func Handle(w http.ResponseWriter, req *http.Request) {
 	id := req.PathValue(api.GameID)
 	if id == "" {
@@ -23,7 +22,6 @@ func Handle(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(apiGame)
 	if err != nil {
-		// The header has already been set. Nothing much we can do here.
 		log.Printf("failed to write response: %s", err)
 	}
 }
