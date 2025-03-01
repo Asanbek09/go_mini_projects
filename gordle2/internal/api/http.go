2d212b099953af2ec431924f399d0841cef1a271
Asanbek09 <samudinua@gmail.com>
2025-03-01 13:01:28 +0600

    internal/api/http.go изменения

--- gordle2/internal/api/http.go
+++ gordle2/internal/api/http.go
@@ -3,3 +3,16 @@ package api
 const (
 	NewGameRoute = "/games"
 )
+
+type GameResponse struct {
+	ID string `json:"attempts_left"`
+	Guesses []Guess `json:"guesses"`
+	WordLength byte `json:"word_length"`
+	Solution string `json:"solution,omitempty"`
+	Status string `json:"status"`
+}
+
+type Guess struct {
+	Word string `json:"word"`
+	Feedback string `json:"feedback"`
+}
