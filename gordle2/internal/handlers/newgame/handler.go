ac29124c82dbc3a328e1ed330696be184749fb39
Asanbek09 <samudinua@gmail.com>
2025-03-03 13:09:23 +0600

    newgame/handler.go -> response

--- gordle2/internal/handlers/newgame/handler.go
+++ gordle2/internal/handlers/newgame/handler.go
@@ -13,7 +13,7 @@ func Handle(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	apiGame := api.ToGameResponse(game)
+	apiGame := api.GameResponse{}
 	err := json.NewEncoder(w).Encode(apiGame)
 	if err != nil {
 		log.Printf("failed to write response: %s", err)
@@ -22,4 +22,8 @@ func Handle(w http.ResponseWriter, req *http.Request) {
 
 func createGame() session.Game {
 	return session.Game{}
+}
+
+func response(game session.Game) api.GameResponse {
+	return api.GameResponse{}
 }
