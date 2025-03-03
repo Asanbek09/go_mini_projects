944a6b3c4b6a890b63a38597bd03cfc4ae0e4cc1
Asanbek09 <samudinua@gmail.com>
2025-03-03 13:46:10 +0600

    handlers/repository -> memory.go -> New

--- gordle2/internal/repository/memory.go
+++ gordle2/internal/repository/memory.go
@@ -5,3 +5,9 @@ import "gordle2/internal/session"
 type GameRepository struct {
 	storage map[session.GameID]session.Game
 }
+
+func New() *GameRepository {
+	return &GameRepository{
+		storage: make(map[session.GameID]session.Game),
+	}
+}
