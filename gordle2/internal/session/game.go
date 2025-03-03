2531a462b6771d2b02fe756e94eb12902bfe4f60
Asanbek09 <samudinua@gmail.com>
2025-03-03 10:12:24 +0600

    internal/session -> game.go изменения

--- gordle2/internal/session/game.go
+++ gordle2/internal/session/game.go
@@ -6,3 +6,18 @@ type Game struct {
 	Guesses []Guess
 	Status Status
 }
+
+type GameID string
+
+type Status string
+
+const (
+	StatusPlaying = "Playing"
+	StatusWon = "Won"
+	StatusLost = "Lost"
+)
+
+type Guess struct {
+	Word string
+	Feedback string
+}
