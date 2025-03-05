a2d8956a23e5a1f58d68e8e25f23e69c6943945d
Asanbek09 <samudinua@gmail.com>
2025-03-05 15:26:11 +0600

    internal/solver -> solver.go -> New

--- mazesolver/internal/solver/solver.go
+++ mazesolver/internal/solver/solver.go
@@ -1,6 +1,9 @@
 package solver
 
-import "image"
+import (
+	"fmt"
+	"image"
+)
 
 type Solver struct {
 	maze *image.RGBA
@@ -8,4 +11,15 @@ type Solver struct {
 
 func (s *Solver) Solve() error {
 	return nil
+}
+
+func New(imagePath string) (*Solver, error) {
+	img, err := openMaze(imagePath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open maze image: %w", err)
+	}
+
+	return &Solver{
+		maze: img,
+	}, nil
 }
