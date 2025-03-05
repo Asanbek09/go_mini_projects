dddfb031eaa4a86c0c9ca688127f45fe8a5e885b
Asanbek09 <samudinua@gmail.com>
2025-03-05 13:57:36 +0600

    imagefile.go создан

---
+++ mazesolver/imagefile.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+)
+
+func openMaze(imagePath string) (*image.RGBA, error) {
+	f, err := os.Open(imagePath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open image %s: %w", imagePath, err)
+	}
+	defer f.Close()
+
+	img, err := png.Decode(f)
+	if err != nil {
+		return nil, fmt.Errorf("unable to load input image from %s: %w", imagePath, err)
+	}
+
+	rgbaImage, ok := img.(*image.RGBA)
+	if !ok {
+		return nil, fmt.Errorf("expected RGBA image, got %T", img)
+	}
+
+	return rgbaImage, nil
+}
