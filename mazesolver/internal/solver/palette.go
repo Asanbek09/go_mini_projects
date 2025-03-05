97a3828ddefaa4750b311040881d53bef21a0f1b
Asanbek09 <samudinua@gmail.com>
2025-03-05 15:51:06 +0600

    internal/solver/paleete.go -> defaultPalette

--- mazesolver/internal/solver/palette.go
+++ mazesolver/internal/solver/palette.go
@@ -9,3 +9,13 @@ type palette struct {
 	treasure color.RGBA
 	solution color.RGBA
 }
+
+func defaultPalette() palette {
+	return palette{
+		wall: color.RGBA{R: 0, G: 0, B: 0, A: 255},
+		path: color.RGBA{R: 255, G: 255, B: 255, A: 255},
+		entrance: color.RGBA{R: 0, G: 191, B: 255, A: 255},
+		treasure: color.RGBA{R: 255, G: 0, B: 128, A: 255},
+		solution: color.RGBA{R: 255, G: 140, B: 0, A: 255},
+	}
+}
