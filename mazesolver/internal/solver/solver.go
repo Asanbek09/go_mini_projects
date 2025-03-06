a979d13e4b4510c29d19ded8977ba30affaaceda
Asanbek09 <samudinua@gmail.com>
2025-03-06 09:41:05 +0600

    solver.go -> findEntrance

--- mazesolver/internal/solver/solver.go
+++ mazesolver/internal/solver/solver.go
@@ -24,3 +24,15 @@ func New(imagePath string) (*Solver, error) {
 		maze: img,
 	}, nil
 }
+
+func (s *Solver) findEntrance() (image.Point, error) {
+	for row := s.maze.Bounds().Min.Y; row < s.maze.Bounds().Max.Y; row++ {
+		for col := s.maze.Bounds().Min.X; col < s.maze.Bounds().Max.X; col++ {
+			if s.maze.RGBAAt(col, row) == s.palette.entrance {
+				return image.Point{X: col, Y: row}, nil
+			}
+		}
+	}
+
+	return image.Point{}, fmt.Errorf("entrance position not found")
+}
