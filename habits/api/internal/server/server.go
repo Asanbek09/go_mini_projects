8d1536215f1abc8832bba9a49277c130e68ee33e
Asanbek09 <samudinua@gmail.com>
2025-03-08 16:34:11 +0600

    server.go -> ListenAndServer

--- habits/api/internal/server/server.go
+++ habits/api/internal/server/server.go
@@ -1,6 +1,17 @@
 package server
 
+import (
+	"fmt"
+	"net"
+	"context"
+	"strconv"
+
+	"google.golang.org/grpc"
+	"habits/api"
+)
+
 type Server struct {
+	api.UnimplementedHabitsServer
 	lgr Logger
 }
 
@@ -10,4 +21,29 @@ func New(lgr Logger) *Server {
 
 type Logger interface {
 	Logf(format string, args ...any)
+}
+
+func (s *Server) ListenAndServer(port int) error {
+	const addr = "127.0.0.1"
+
+	listener, err := net.Listen("tcp", net.JoinHostPort(addr, strconv.Itoa(port)))
+	if err != nil {
+		return fmt.Errorf("unable to listen to tcp port %d: %w", port, err)
+	}
+
+	grpcServer := grpc.NewServer()
+	api.RegisterHabitsServer(grpcServer, s)
+
+	s.lgr.Logf("starting server on port %d\n", port)
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		return fmt.Errorf("error while listening: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Server) CreateHabit(ctx context.Context, req *api.CreateHabitRequest) (*api.CreateHabitResponse, error) {
+	return &api.CreateHabitResponse{}, nil
 }
