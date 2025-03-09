5041cf09d37d99e695d1032be20f64b364ae499f
Asanbek09 <samudinua@gmail.com>
2025-03-09 13:18:31 +0600

    create_internal_test.go -> testValidateAndFillDetailsFull

--- habits/api/pkg/habit/create_internal_test.go
+++ habits/api/pkg/habit/create_internal_test.go
@@ -2,6 +2,10 @@ package habit
 
 import (
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_validateAndFillDetails(t testing.T) {
@@ -10,4 +14,19 @@ func Test_validateAndFillDetails(t testing.T) {
 	t.Run("Full", testValidateAndFillDetailsFull)
 	t.Run("Partial", testValidateAndFillDetailsPartial)
 	t.Run("SpaceName", testValidateAndFillDetailsSpaceName)
+}
+
+func testValidateAndFillDetailsFull(t testing.T) {
+	t.Parallel()
+
+	h := Habit{
+		ID: "987",
+		Name: "laugh",
+		WeeklyFrequency: 256,
+		CreationTime: time.Date(2024, 03, 9, 1, 5, 0, 0, time.UTC),
+	}
+
+	got, err := validateAndFillDetails(h)
+	require.NoError(t, err)
+	assert.Equal(t, h, got)
 }
